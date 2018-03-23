a407d31314f4582d0968f822296db1a67e01e16e
Piotrek <Piotrek@PiotrekNB>
2018-03-23 19:56:07 +0100

    initial commit

---
+++ PassGuard/src/main/java/com/passguard/PassGuardApplication.java
@@ -0,0 +1,9 @@
+package com.passguard;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class PassGuardApplication extends Application {
+
+}
